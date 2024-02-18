7943d3b12c79ffb6d24938747782fccd442eac71
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-02-18 20:36:09 +0530

    routing

---
+++ src/styledpractice.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const styledpractice = () => {
+  return (
+    <Wrapper>
+      <div>styledpractice</div>;
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  h3 {
+    color: red;
+  }
+`;
+export default styledpractice;
